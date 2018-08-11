0b49a9991f5efd715f990f8676339aeaf8ed8bf8
AlexanderAlexAlexanderAlex <alex288k@gmail.com>
2018-08-11 12:59:58 +0200

    Hoffnungslose versuche

--- app/src/main/java/org/jugendhackt/fahrradkette/MainActivity.java
+++ app/src/main/java/org/jugendhackt/fahrradkette/MainActivity.java
@@ -1,5 +1,6 @@
 package org.jugendhackt.fahrradkette;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final Intent intent = new Intent(this, NewPos.class);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(intent);
             }
         });
     }
