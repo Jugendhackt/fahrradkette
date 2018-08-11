90f614eff2d2efcd54632138badd50ffe64f3425
Ulf-vom-Mond <noahrullmann@gmail.com>
2018-08-11 20:49:47 +0200

    beautiful map, zoomed in to actual position and some markers

--- app/src/main/java/org/jugendhackt/fahrradkette/MainActivity.java
+++ app/src/main/java/org/jugendhackt/fahrradkette/MainActivity.java
@@ -1,6 +1,7 @@
 package org.jugendhackt.fahrradkette;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
@@ -11,18 +12,38 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.OverlayItem;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
     MapView map = null;
+    double latPos = 51.47931;
+    double lonPos = 11.99317;
+    double latBike0 = 51.48991;
+    double lonBike0 = 12.015;
+    double latBike1 = 51.46951;
+    double lonBike1 = 11.98287;
+    double latBike2 = 51.48991;
+    double lonBike2 = 11.98257;
+    double latBike3 = 51.46971;
+    double lonBike3 = 12.01;
+    MyLocationNewOverlay mLocationOverlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -41,7 +62,60 @@ public class MainActivity extends AppCompatActivity {
 
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom(16);
+        this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(this),map);
+        this.mLocationOverlay.enableMyLocation();
+        map.getOverlays().add(this.mLocationOverlay);
+        GeoPoint startPoint = new GeoPoint(latPos, lonPos);
 
+        mapController.setCenter(startPoint);
+
+        Marker bike0 = new Marker(map);
+        GeoPoint bikePos0 = new GeoPoint(latBike0, lonBike0);
+        bike0.setPosition(bikePos0);
+        bike0.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(bike0);
+
+        Marker bike1 = new Marker(map);
+        GeoPoint bikePos1 = new GeoPoint(latBike1, lonBike1);
+        bike1.setPosition(bikePos1);
+        bike1.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(bike1);
+
+        Marker bike2 = new Marker(map);
+        GeoPoint bikePos2 = new GeoPoint(latBike2, lonBike2);
+        bike2.setPosition(bikePos2);
+        bike2.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(bike2);
+
+        Marker bike3 = new Marker(map);
+        GeoPoint bikePos3 = new GeoPoint(latBike3, lonBike3);
+        bike3.setPosition(bikePos3);
+        bike3.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(bike3);
+
+        ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+        items.add(new OverlayItem("Title", "Description", new GeoPoint(51.465,11.985))); // Lat/Lon decimal degrees
+
+//the overlay
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+                        //do something
+                        return true;
+                    }
+                    @Override
+                    public boolean onItemLongPress(final int index, final OverlayItem item) {
+                        return false;
+                    }
+                }, ctx);
+        mOverlay.setFocusItemsOnTap(true);
+
+        map.getOverlays().add(mOverlay);
     }
 
     public void onResume(){
@@ -60,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
         //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -81,4 +156,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
